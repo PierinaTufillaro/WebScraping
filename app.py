9da88ca0e2398e7242bd9dd966ebb240d73b6e6b
PierinaTufillaro <pierinatufillaro@hotmail.com>
2020-09-17 08:54:15 -0300

    Agregue comentarios y una estadistica mas

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify, request 
 
-#Para encontrar los meta
+#To work with meta tags
 import requests
 from bs4 import BeautifulSoup
 from requests import get
 
-#Para tratar archivos csv
+#To work with .csv files
 import csv
 import codecs
 
+#To get the keyword with maximum appearances
+import operator
+
 app = Flask(__name__)
 
+#Counts the amount of keywords that the title has
 def keywordsInTitle(kList,title):
 	count = 0
 	for keyword in kList:
@@ -18,12 +22,14 @@ def keywordsInTitle(kList,title):
 			count = count + 1
 	return count
 
+#Gets the frequency of keywords
 def countingKeywords(klist):
 	freq = {} 
 	for items in klist:
 		freq[items] = klist.count(items) 
 	return freq
 
+#Returns the keywords that appears once
 def uniqueKeywords(klist):
 	result = []
 	for items in klist:
@@ -31,19 +37,24 @@ def uniqueKeywords(klist):
 			result.append(items)
 	return len(result)
 
-
+#This endpoint gets the keywords of a webpage that gets from parameter and returns the statistics
+#In some pages are stored in a meta tag named 'kewords', and in others with name = 'keyword'
 @app.route('/keywords', methods=['POST'])
 def keywords():
 	req_Json = request.get_json() 
 	page = req_Json['page']
 	data = {}
 	try:
+		#Gets the keywords
 		res = requests.get(page).text
 		soup = BeautifulSoup(res, "html.parser")
 		title = soup.find('title').string
 		meta_list= soup.find_all('meta',{'name':'keywords'})
+		#If it doesn't find anything, search with 'keyword' name
 		if not meta_list:
 			meta_list= soup.find_all('meta',{'name':'keyword'})
+		#If meta_list is not empty, it will get all the keywords
+		#and then make the statistics
 		if meta_list:
 			for a in meta_list:
 				content = a.get('content')
@@ -54,8 +65,8 @@ def keywords():
 				data['Keywords In Title']= 'No title'		
 			keywordsFrequency = countingKeywords(lista)
 			data['Keywords Frequency'] = keywordsFrequency
-			keywordsOnce =  uniqueKeywords(keywordsFrequency)
-			data['Unique Keywords'] = keywordsOnce
+			data['Unique Keywords'] = uniqueKeywords(keywordsFrequency)
+			data['Keyword with maximum appearances'] =  max(keywordsFrequency.items(), key=operator.itemgetter(1))[0]
 		else:
 			data['Keywords In Title']= 'No keywords'
 			data['Keywords Frequency'] = 'No keywords'
@@ -64,6 +75,7 @@ def keywords():
 	except Exception as exception:
 			return ("Ocurrio una excepcion: " + str(exception))	
 
+#This endpoint returns the title of a webpage send by a parameter
 @app.route('/title', methods=['POST'])
 def title():
 	req_Json = request.get_json() 
@@ -78,6 +90,8 @@ def title():
 	except Exception as exception:
 		return ("Ocurrio una excepcion: " + str(exception))
 
+#This endpoint process a .csv file that contents urls.
+#It returns the title of those urls.
 @app.route('/csv_reader')
 def csv_reader(file_obj):
 	reader = csv.reader(file_obj)
@@ -97,7 +111,11 @@ def csv_reader(file_obj):
 	print ("ok")
 
 if __name__ == '__main__':
-	csv_path = 'sample_urls.csv'
-	with open(csv_path, "rb") as f_obj:
-		csv_reader(codecs.iterdecode(f_obj, 'utf-8'))
+
+#Delete the following pound keys to try the endpoint that process
+#the .csv files
+
+#	csv_path = 'sample_urls.csv'
+#	with open(csv_path, "rb") as f_obj:
+#		csv_reader(codecs.iterdecode(f_obj, 'utf-8'))
 	app.run(debug=True, port=5000)
